87548ae510793e50f45b3339eb312f8f77c582be
Faell4328 <rhafahenrique@hotmail.com>
2025-10-26 11:47:36 -0300

    corrigindo workflow do github action

--- publish.js
+++ publish.js
@@ -1,24 +1,6 @@
 import { execSync } from 'child_process';
-import readline from 'readline';
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
-
-rl.question('Caso queria coloca uma mensagem complementar: ', (mensagem) => {
-
-    execSync('git checkout -f main');
-    if(mensagem != ""){
-        execSync('git merge "draft"');
-    }
-    else{
-        console.log("A mensagem é obrigatória");
-        execSync('git checkout -f draft');
-        process.exit(0);
-    }
-
-    execSync('git push');
-    execSync('git checkout -f draft');
-    process.exit(0);    
-});
+execSync('git checkout -f main');
+execSync('git merge "draft"');
+execSync('git push');
+execSync('git checkout -f main');
