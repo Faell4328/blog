5a93fcbfeab3d66133258be976691f23d2472ba5
Faell4328 <rhafahenrique@hotmail.com>
2025-10-26 17:34:00 -0300

    ajustando os links para o novo domínio

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://faell4328.github.io/blog/',
+	site: 'https://rhafaell.com.br',
 	integrations: [mdx(), sitemap()]
 });
--- src/content/blog/OWASP.md
+++ src/content/blog/OWASP.md
@@ -1,6 +1,6 @@
 ---
 title: 'OWASP Top 10: o guia essencial para desenvolvedores'
-description: '/blog/imagens/owasp.png'
+description: '/imagens/owasp.png'
 pubDate: '10/26/2025'
 ---
 
