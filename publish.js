321090ff29cf5d3b0bb4b7b58c636730a19d1e6a
Faell4328 <rhafahenrique@hotmail.com>
2025-10-27 15:51:48 -0300

    publish - 27/10/2025 - 15:51:48 - adicionando script no github actions e alterando arquivo de publish.js

--- publish.js
+++ publish.js
@@ -1,6 +1,30 @@
 import { execSync } from 'child_process';
+import readline from 'readline';
 
-execSync('git checkout -f main');
-execSync('git merge "draft"');
-execSync('git push');
-execSync('git checkout -f draft');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.question('Caso queria coloca uma mensagem complementar: ', (mensagem) => {
+
+    const d = new Date();
+    const data_formatada = `${(d.getDate().toString()).padStart(2, '0')}/${((d.getMonth()+1).toString()).padStart(2, '0')}/${d.getFullYear()} - ${(d.getHours().toString()).padStart(2, '0')}:${(d.getMinutes().toString()).padStart(2, '0')}:${(d.getSeconds().toString()).padStart(2, '0')}`
+
+    execSync('git add .');
+    if(mensagem != ""){
+        execSync(`git commit -m 'publish - ${data_formatada} - ${mensagem.toString()}'`);
+    }
+    else{
+        execSync(`git commit -m 'publish - ${data_formatada}'`);
+    }
+    
+    execSync(`git push --set-upstream origin draft`);
+    
+    execSync('git checkout -f main');
+    execSync('git merge "draft"');
+    execSync('git push');
+    execSync('git checkout -f draft');
+    console.log("Publicação feita");
+    process.exit(0);
+});
